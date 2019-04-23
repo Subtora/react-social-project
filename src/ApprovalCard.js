aee581ff9370f9ccaca1454a1a3758206000c8ca
unknown <16tsmith55@gmail.com>
2019-04-23 15:02:18 -0600

    attempt to fix pages access

---
+++ src/ApprovalCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ApprovalCard = () => {
+  return (
+    <>
+      <div className="ui card">
+        <div className="content"> </div>
+      </div>
+    </>
+  );
+};
+export default ApprovalCard;
