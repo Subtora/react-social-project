635daf0cf84561362faec9f59da2ff21c0d0aaae
unknown <16tsmith55@gmail.com>
2019-05-01 20:24:34 -0600

    updated AprovalCard.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Comment from "./Comment";
+import ApprovalCard from "./ApprovalCard";
 import faker from "faker";
 import * as serviceWorker from "./serviceWorker";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <Comment
-        author={faker.name.firstName()}
-        avatar={faker.image.avatar()}
-        content={faker.random.words()}
-        date={`${faker.date
-          .past()
-          .getHours()}:${faker.date.past().getMinutes()}`}
-      />
-      <Comment
-        author={faker.name.firstName()}
-        avatar={faker.image.avatar()}
-        content={faker.random.words()}
-        date={`${faker.date
-          .past()
-          .getHours()}:${faker.date.past().getMinutes()}`}
-      />
-      <Comment
-        author={faker.name.firstName()}
-        avatar={faker.image.avatar()}
-        content={faker.random.words()}
-        date={`${faker.date
-          .past()
-          .getHours()}:${faker.date.past().getMinutes()}`}
-      />
+      <ApprovalCard>
+        <Comment
+          author={faker.name.firstName()}
+          avatar={faker.image.avatar()}
+          content={faker.random.words()}
+          date={`${faker.date
+            .past()
+            .getHours()}:${faker.date.past().getMinutes()}`}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment
+          author={faker.name.firstName()}
+          avatar={faker.image.avatar()}
+          content={faker.random.words()}
+          date={`${faker.date
+            .past()
+            .getHours()}:${faker.date.past().getMinutes()}`}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment
+          author={faker.name.firstName()}
+          avatar={faker.image.avatar()}
+          content={faker.random.words()}
+          date={`${faker.date
+            .past()
+            .getHours()}:${faker.date.past().getMinutes()}`}
+        />
+      </ApprovalCard>
     </div>
   );
 };
